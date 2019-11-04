87e0c816ecad97644a833f45d932892ee8fac2fa
Samuele Torregrossa <samuele_t95@hotmail.com>
2019-11-04 12:57:55 +0100

    I'm bored

--- phase2/Tetris/src/UI.java
+++ phase2/Tetris/src/UI.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
-        import java.awt.*;
-        import java.awt.geom.Rectangle2D;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 /**
  * This class takes care of all the graphics to display a certain state of the game field
@@ -63,27 +63,29 @@ public class UI extends JPanel
         {
             for (int j = 0; j < state[0].length; j++)
             {
-                localGraphics2D.setColor(GetColorOfID(state[i][j]));
+                int n=150;
+                if(Tetris.tempField[i][j+5]!=Tetris.field[i][j+5]) n=255;
+                localGraphics2D.setColor(GetColorOfID(state[i][j],n));
                 localGraphics2D.fill(new Rectangle2D.Double(i * size + 1, j * size + 1, size - 1, size - 1));
             }
         }
     }
 
     // Decodes the ID of a pentomino into a color
-    private Color GetColorOfID(int i)
+    private Color GetColorOfID(int i, int a)
     {
-        if(i==0) {return Color.BLUE;}
-        else if(i==1) {return Color.ORANGE;}
-        else if(i==2) {return Color.CYAN;}
-        else if(i==3) {return Color.GREEN;}
-        else if(i==4) {return Color.MAGENTA;}
-        else if(i==5) {return Color.PINK;}
-        else if(i==6) {return Color.RED;}
-        else if(i==7) {return Color.YELLOW;}
-        else if(i==8) {return new Color(0, 0, 0);}
-        else if(i==9) {return new Color(0, 0, 100);}
-        else if(i==10) {return new Color(100, 0,0);}
-        else if(i==11) {return new Color(0, 100, 0);}
+        if(i==0) {return new Color(0, 0, 255, a);}
+        else if(i==1) {return new Color(255, 102, 0, a);}
+        else if(i==2) {return new Color(0, 255, 255, a);}
+        else if(i==3) {return new Color(0, 204, 0, a);}
+        else if(i==4) {return new Color(255, 0, 255, a);}
+        else if(i==5) {return new Color(255, 51, 255, a);}
+        else if(i==6) {return new Color(255, 0, 0, a);}
+        else if(i==7) {return new Color(255, 255, 0, a);}
+        else if(i==8) {return new Color(0, 0, 0, a);}
+        else if(i==9) {return new Color(0, 0, 100, a);}
+        else if(i==10) {return new Color(100, 0, 0, a);}
+        else if(i==11) {return new Color(0, 100, 0, a);}
         else {return Color.LIGHT_GRAY;}
     }
 
