b85229e2e919099bb99d17776a6d36408f875a56
MaxPersoon <56160760+MaxPersoon@users.noreply.github.com>
2019-11-04 19:47:15 +0100

    Added random piece generation :) (with a little bit of help from Rene)

--- src/java/Phase2/Tetris/Game.java
+++ src/java/Phase2/Tetris/Game.java
@@ -6,9 +6,130 @@ public class Game {
     private int[][] nextPiece;
 
     private int[][] generatePiece(){
-        //get a random value and chose the corresponding piece
-        int [][] a=new int[1][1];
-        return a;
+        int random = (int) (12 * Math.random() + 1);
+        int[][] randomPiece;
+
+        switch(random) {
+            case 1:
+                // P
+                randomPiece = new int[][]
+                        {
+                                {1, 1},
+                                {1, 1},
+                                {1, 0}
+                        };
+                break;
+            case 2:
+                // X
+                randomPiece = new int[][]
+                        {
+                                {0,1,0},
+                                {1,1,1},
+                                {0,1,0}
+                        };
+                break;
+            case 3:
+                // F
+                randomPiece = new int[][]
+                        {
+                                {0,1,1},
+                                {1,1,0},
+                                {0,1,0}
+                        };
+                break;
+            case 4:
+                // V
+                randomPiece = new int[][]
+                        {
+                                {1,0,0},
+                                {1,0,0},
+                                {1,1,1}
+                        };
+                break;
+            case 5:
+                // W
+                randomPiece = new int[][]
+                        {
+                                {1,0,0},
+                                {1,1,0},
+                                {0,1,1}
+                        };
+                break;
+            case 6:
+                // Y
+                randomPiece = new int[][]
+                        {
+                                {0,1},
+                                {1,1},
+                                {0,1},
+                                {0,1}
+                        };
+                break;
+            case 7:
+                // I
+                randomPiece = new int[][]
+                        {
+                                {1},
+                                {1},
+                                {1},
+                                {1},
+                                {1}
+                        };
+                break;
+            case 8:
+                // L
+                randomPiece = new int[][]
+                        {
+                                {0,0,0,1},
+                                {1,1,1,1},
+                        };
+                break;
+            case 9:
+                // N
+                randomPiece = new int[][]
+                        {
+                                {1,1,0,0},
+                                {0,1,1,1}
+                        };
+                break;
+            case 10:
+                // U
+                randomPiece = new int[][]
+                        {
+                                {1,0,1},
+                                {1,1,1},
+                        };
+                break;
+            case 11:
+                // Z
+                randomPiece = new int[][]
+                        {
+                                {1,1,0},
+                                {0,1,0},
+                                {0,1,1}
+                        };
+                break;
+            case 12:
+                // T
+                randomPiece = new int[][]
+                        {
+                                {1,1,1},
+                                {0,1,0},
+                                {0,1,0}
+                        };
+                break;
+            default:
+                // null
+                randomPiece = new int[][]
+                        {
+                                {0,0,0},
+                                {0,0,0},
+                                {0,0,0}
+                        };
+                break;
+        }
+
+        return randomPiece;
     }
 
     public void increaseScore(int increaseBy){
