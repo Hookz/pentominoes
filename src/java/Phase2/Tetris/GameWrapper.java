7ed5e7c9f50a3eeef78d7788a2cc8dd4a1a0607c
Michal Jarski <michal@jarski.eu>
2019-11-04 16:46:48 +0100

    Hope this won't break anything :)

---
+++ src/java/Phase2/Tetris/GameWrapper.java
@@ -0,0 +1,63 @@
+package Phase2.Tetris;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWrapper extends JPanel{
+    public JLabel score = new JLabel(number(Tetris.score), JLabel.CENTER);
+    public JLabel[] gamePieces = new Phase1.ChoosePieces().pieces;
+    public JFrame window;
+    public static UI ui;
+    public JLabel nextPiece;
+
+    public GameWrapper(int x, int y, int _size){
+        score.setFont(new Font("Comic Sans MS", Font.BOLD, 22));
+        ui = new UI(x,y,_size);
+        ui.setBounds(0,0,x*_size,y*_size);
+        window = new JFrame("Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new GridLayout(1,2));
+        window.add(ui);
+
+        JPanel rightPanel = new JPanel(new GridLayout(3,1));
+        JPanel scorePanel = new JPanel(new GridLayout(2,1));
+        JPanel nextPiecePanel = new JPanel(new GridLayout(2,1));
+
+        JLabel scoreLabel = new JLabel("Score",JLabel.CENTER);
+        scoreLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+
+        scorePanel.add(scoreLabel);
+        scorePanel.add(score);
+
+        JLabel nextPieceLabel = new JLabel("Next piece", JLabel.CENTER);
+        nextPieceLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 22));
+        for(int i=0;i<gamePieces.length;i++){
+            gamePieces[i].setHorizontalAlignment(JLabel.CENTER);
+            gamePieces[i].setBackground(Color.white);
+        }
+        // TODO: To be set to randomly generated ID number used for randomizing game pieces
+        nextPiece = gamePieces[2];
+
+        nextPiecePanel.add(nextPieceLabel);
+        nextPiecePanel.add(nextPiece);
+
+        rightPanel.add(scorePanel);
+        rightPanel.add(nextPiecePanel);
+
+        window.add(rightPanel);
+        window.pack();
+        window.setResizable(false);
+        window.setVisible(true);
+
+        /*Example update method for nextPiece.
+        It can be accessed by other classes through the Tetris class as Tetris.gameWrapper.nextPiece.
+        Useful for changing the piece dynamically after the previous one has already been placed.
+        Cannot be achieved by nextPiece = gamePieces[i] since it would break the reference.
+        */
+        nextPiece.setIcon(gamePieces[7].getIcon());
+    }
+    private static String number(int x){
+        String sequence = Integer.toString(x);
+        return sequence;
+    }
+}
