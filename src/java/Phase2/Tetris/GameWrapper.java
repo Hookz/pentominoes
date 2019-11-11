86ea4264c382b9b1ee50cfb4b66f853aabcafef1
Michal Jarski <michal@jarski.eu>
2019-11-11 20:21:30 +0100

    Fixed bug with the Label being misused in GameWrapper
    Signed-off-by: Michal Jarski <michal@jarski.eu>

--- src/java/Phase2/Tetris/GameWrapper.java
+++ src/java/Phase2/Tetris/GameWrapper.java
@@ -37,8 +37,8 @@ public class GameWrapper extends JPanel{
             gamePieces[i].setBackground(Color.white);
         }
 
-        // TODO: To be set to randomly generated ID number used for randomizing game pieces
-        nextPiece = gamePieces[2];
+        nextPiece = new JLabel();
+        nextPiece.setHorizontalAlignment(JLabel.CENTER);
 
         nextPiecePanel.add(nextPieceLabel);
         nextPiecePanel.add(nextPiece);
