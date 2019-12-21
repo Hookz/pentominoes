b3626d1c4f3aa409128eed7da433761c9b2eb5d4
René Steeman <steeman.rene1@gmail.com>
2019-12-21 12:11:26 +0100

    Created basis for pentomino rotations and different pentominoes (requires a lot of boring work to finish, but it's pretty easy)

--- phase 3/code/src/Pentomino.java
+++ phase 3/code/src/Pentomino.java
@@ -11,6 +11,9 @@ public class Pentomino{
     final double yStart;
     final double zStart;
 
+    final char type;
+    final int rotation;
+
     PhongMaterial item_material = new PhongMaterial();
     Color item_color;
     Group shape = new Group();
@@ -21,7 +24,7 @@ public class Pentomino{
     //ONLY L, P, T PENTOMINOES ARE REQUIRED
     //TODO add rotations
     //L
-    public Pentomino(double xStart, double yStart, double zStart) {
+    public Pentomino(double xStart, double yStart, double zStart, char type, int rotation) {
         this.xStart = xStart;
         this.yStart = yStart;
         this.zStart = zStart;
@@ -30,17 +33,46 @@ public class Pentomino{
         this.yCenter = yStart + CEL_SIZE/2;
         this.zCenter = zStart + CEL_SIZE/2;
 
+        this.type = type;
+        this.rotation = rotation;
+
         item_color = Color.rgb((int) (Math.random()*255), (int) (Math.random()*255), (int) (Math.random()*255), 1);
         item_material.setDiffuseColor(item_color);
 
-        //create cells and move them
-        for(int i=0; i<5; i++){
-            cells[i] = new Box(CEL_SIZE, CEL_SIZE, CEL_SIZE);
+        /*Rotations are as follows
+        1: No rotation
+        2: Turn clockwise 90
+        3: Turn clockwise 180
+        4: Turn clockwise 270
+        5: Make it orthogonal to the back
+        6: Make it orthogonal to the front
+         */
 
-            cells[i].setTranslateX(xCenter);
-            cells[i].setTranslateY(yCenter + i*CEL_SIZE);
-            cells[i].setTranslateZ(zCenter);
-            FX3D.createBoxLines(CEL_SIZE, CEL_SIZE, CEL_SIZE, xStart, yStart+i*CEL_SIZE, zStart);
+        switch (type){
+            case 'L':
+                switch (rotation) {
+                    case 1:
+                        //create cells and move them
+                        for(int i=0; i<5; i++){
+                            cells[i] = new Box(CEL_SIZE, CEL_SIZE, CEL_SIZE);
+
+                            cells[i].setTranslateX(xCenter);
+                            cells[i].setTranslateY(yCenter + i*CEL_SIZE);
+                            cells[i].setTranslateZ(zCenter);
+                            FX3D.createBoxLines(CEL_SIZE, CEL_SIZE, CEL_SIZE, xStart, yStart+i*CEL_SIZE, zStart);
+                        }
+
+                        break;
+                }
+
+                break;
+
+            case 'P':
+                break;
+            case 'T':
+                break;
+
+
         }
 
         //add cells to the group
@@ -49,8 +81,5 @@ public class Pentomino{
             shape.getChildren().add(cells[i]);
             FX3D.contentGroup.getChildren().add(cells[i]);
         }
-
     }
-
-
 }
