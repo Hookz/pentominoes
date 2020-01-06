caee05ed8e73cc09b702d463785bcac3b6ecf2d7
renesteeman <steeman.rene1@gmail.com>
2020-01-06 08:13:44 +0100

    Single comment added

--- phase 3/code/src/Algorithm.java
+++ phase 3/code/src/Algorithm.java
@@ -2,6 +2,7 @@ public class Algorithm {
     //TODO create the algorithm that actually solves the problem, make a pentomino and parcel option
 
     //create a container (as ints) for which the problem can be solved, not for the UI (this is done separately)
+    //TODO save items as follows: typeId + uniqueId, this would allow you to seperate all of the elements.
     int[][][] container = new int[Wrapper.CONTAINER_WIDTH][Wrapper.CONTAINER_HEIGHT][Wrapper.CONTAINER_DEPTH];
 
     public void startAlgorithm(){
