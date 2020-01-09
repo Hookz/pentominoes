1a62c223966084e312bd141d98c93926f156583f
renesteeman <steeman.rene1@gmail.com>
2020-01-09 14:55:37 +0100

    Starting on AlgX

---
+++ phase 3/code/src/AlgX.java
@@ -0,0 +1,8 @@
+//https://www.geeksforgeeks.org/exact-cover-problem-algorithm-x-set-2-implementation-dlx/ was used
+//https://stackoverflow.com/questions/39939710/java-how-to-implement-dancing-links-algorithm-with-doublylinkedlists
+//https://github.com/benfowler/dancing-links/tree/master/dlx/src
+//https://github.com/Warren-Partridge/algorithm-x/blob/master/DancingLinks.java
+
+public class AlgX {
+    Node header = new Node();
+}
