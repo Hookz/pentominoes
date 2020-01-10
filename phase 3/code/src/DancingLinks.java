8fb7c8d9d5e7a700503ddeecbe71e1af38f9ea8a
renesteeman <steeman.rene1@gmail.com>
2020-01-10 09:11:08 +0100

    Working on research

--- phase 3/code/src/DancingLinks.java
+++ phase 3/code/src/DancingLinks.java
@@ -1,167 +1,250 @@
-// Data structures implemented from pg. 5 of Dancing Links paper
+// Author: Rafal Szymanski <me@rafal.io>
 
-// Ok, just so it makes sense to me:
-// Data Object = a dancing link. It has 2 circularly linked lists and a list header.
-// Column Object = a column (a set of 1s that we may want to have in our exact cover). It contains DOs and has a name and # of DOs.
-
-import java.util.List;
+// Implementation of the Dancing Links algorithm for exact cover.
+import java.util.*;
 
 public class DancingLinks {
-    class DataObject {
-        DataObject left, right, up, down;
-        ColumnObject C; // Knuth calls this property a "list header" in his paper but I am conflating the two objects into one for simplicity
 
-        DataObject() {
-            left = right = up = down = this;
+    static final boolean verbose = true;
+
+    class DancingNode{
+        DancingNode L, R, U, D;
+        ColumnNode C;
+
+        // hooks node n1 `below` current node
+        DancingNode hookDown(DancingNode n1){
+            assert (this.C == n1.C);
+            n1.D = this.D;
+            n1.D.U = n1;
+            n1.U = this;
+            this.D = n1;
+            return n1;
         }
 
-        DataObject(ColumnObject initC) {
+        // hooke a node n1 to the right of `this` node
+        DancingNode hookRight(DancingNode n1){
+            n1.R = this.R;
+            n1.R.L = n1;
+            n1.L = this;
+            this.R = n1;
+            return n1;
+        }
+
+        void unlinkLR(){
+            this.L.R = this.R;
+            this.R.L = this.L;
+            updates++;
+        }
+
+        void relinkLR(){
+            this.L.R = this.R.L = this;
+            updates++;
+        }
+
+        void unlinkUD(){
+            this.U.D = this.D;
+            this.D.U = this.U;
+            updates++;
+        }
+
+        void relinkUD(){
+            this.U.D = this.D.U = this;
+            updates++;
+        }
+
+        public DancingNode(){
+            L = R = U = D = this;
+        }
+
+        public DancingNode(ColumnNode c){
             this();
-            C = initC;
-            // TODO: Stick on bottom of col by default?
+            C = c;
         }
-
-        DataObject appendToRow(DataObject newDO) { // Append new DO to the right and return it
-            this.right.left = newDO;
-            newDO.left = this;
-            newDO.right = this.right;
-            this.right = newDO;
-
-            return newDO;
-        }
-
-        DataObject appendToColumn(DataObject newDO) { // Append new DO below and return it
-            this.down.up = newDO;
-            newDO.up = this;
-            newDO.down = this.down;
-            this.down = newDO;
-
-            return newDO;
-        }
-
-        void unlinkFromRow() {
-            this.left.right = this.right;
-            this.right.left = this.left;
-        }
-
-        void relinkToRow() {
-            this.left.right = this.right.left = this;
-        }
-
-        void unlinkFromColumn() {
-            this.up.down = this.down;
-            this.down.up = this.up;
-            this.C.size--;
-        }
-
-        void relinkToColumn() {
-            this.up.down = this.down.up = this;
-            this.C.size++;
-        }
-
     }
 
-    class ColumnObject extends DataObject {
-        //DataObject left, right, up, down;
-        //ColumnObject C;
+    class ColumnNode extends DancingNode{
+        int size; // number of ones in current column
         String name;
-        int size; // Number of 1s in the column
 
-        ColumnObject(String initName) {
-            super(); // Inherit left,right,up,down,C from dataobject
-            C = this;
-            name = initName;
+        public ColumnNode(String n){
+            super();
             size = 0;
+            name = n;
+            C = this;
         }
 
-        void cover() {
-            this.unlinkFromRow();
-
-            for (DataObject i = this.down; i != this.C; i = i.down) {
-                for (DataObject j = i.right; j != i; j = j.right) {
-                    j.unlinkFromColumn();
+        void cover(){
+            unlinkLR();
+            for(DancingNode i = this.D; i != this; i = i.D){
+                for(DancingNode j = i.R; j != i; j = j.R){
+                    j.unlinkUD();
+                    j.C.size--;
                 }
             }
+            header.size--; // not part of original
         }
 
-        void uncover() {
-            for (DataObject i = this.up; i != this.C; i = i.up) {
-                for (DataObject j = i.left; j != i; j = j.left) {
-                    j.relinkToColumn();
+        void uncover(){
+            for(DancingNode i = this.U; i != this; i = i.U){
+                for(DancingNode j = i.L; j != i; j = j.L){
+                    j.C.size++;
+                    j.relinkUD();
                 }
             }
-
-            this.relinkToRow();
+            relinkLR();
+            header.size++; // not part of original
         }
-
     }
 
-    private ColumnObject root; // Special CO, labeled "h" in the paper
-    private List<DataObject> solutions;
-    private int numSolutionsFound = 0;
+    private ColumnNode header;
+    private int solutions = 0;
+    private int updates = 0;
+    private List<DancingNode> answer;
 
-    private void search(int K) { // Deterministic algorithm to find all exact covers
-        if (root.right == root) {
-            numSolutionsFound++;
-
-            // TODO: Print the current solution
-
-            return;
-        }
-
-        ColumnObject c = getSmallestColumnObject();
-        c.cover();
-
-        for (DataObject r = c.down; r != c; r = r.down) {
-            solutions.add(r);
-
-            for (DataObject j = r.right; j != r; j = j.right) {
-                j.C.cover();
+    // Heart of the algorithm
+    private void search(int k){
+        if (header.R == header){ // all the columns removed
+            if(verbose){
+                System.out.println("-----------------------------------------");
+                System.out.println("Solution #" + solutions + "\n");
             }
-
-            search(K + 1);
-            r = solutions.remove(solutions.size() - 1);
-            c = r.C;
-
-            for (DataObject j = r.left; j != r; j = j.left) {
-                j.C.uncover();
+            if(verbose){
+                System.out.println("-----------------------------------------");
             }
+            solutions++;
+        } else{
+            ColumnNode c = selectColumnNodeHeuristic();
+            c.cover();
+
+            for(DancingNode r = c.D; r != c; r = r.D){
+                answer.add(r);
+
+                for(DancingNode j = r.R; j != r; j = j.R){
+                    j.C.cover();
+                }
+
+                search(k + 1);
+
+                r = answer.remove(answer.size() - 1);
+                c = r.C;
+
+                for(DancingNode j = r.L; j != r; j = j.L){
+                    j.C.uncover();
+                }
+            }
+            c.uncover();
         }
-
-        c.uncover();
-
-        return;
-
-        // Pseudocode from the paper:
-
-//        If R[h] = h, print the current solution (see below) and return.
-//        Otherwise choose a column object c (see below).
-//        Cover column c (see below).
-//        For each r ← D[c], D[D[c]], ... , while r != c,
-//          set Ok ← r;
-//          for each j ← R[r], R[R[r]], ... , while j != r,
-//            cover column j (see below);
-//          search(k + 1);
-//          set r ← Ok and c ← C[r];
-//          for each j ← L[r], L[L[r]], ... , while j != r,
-//            uncover column j (see below).
-//        Uncover column c (see below) and return.
     }
 
-    private ColumnObject getSmallestColumnObject(ColumnObject root) {
+    private ColumnNode selectColumnNodeNaive(){
+        return (ColumnNode) header.R;
+    }
+
+    private ColumnNode selectColumnNodeHeuristic(){
         int min = Integer.MAX_VALUE;
-        ColumnObject smallestCO = null;
+        ColumnNode ret = null;
+        for(ColumnNode c = (ColumnNode) header.R; c != header; c = (ColumnNode) c.R){
+            if (c.size < min){
+                min = c.size;
+                ret = c;
+            }
+        }
+        return ret;
+    }
 
-        // Search for the min size CO by iterating through all COs by moving right until we end up back at the header
-        //every node in headers
-        for(ColumnObject col = (ColumnObject) root.right; col != root; col = (ColumnObject) col.right){
-            if (col.size < min) {
-                min = col.size;
-                smallestCO = col;
+    // Ha, another Knuth algorithm
+    private ColumnNode selectColumnNodeRandom(){ // select a column randomly
+        ColumnNode ptr = (ColumnNode) header.R;
+        ColumnNode ret = null;
+        int c = 1;
+        while(ptr != header){
+            if(Math.random() <= 1/(double)c){
+                ret = ptr;
+            }
+            c++;
+            ptr = (ColumnNode) ptr.R;
+        }
+        return ret;
+    }
+
+    private ColumnNode selectColumnNodeNth(int n){
+        int go = n % header.size;
+        ColumnNode ret = (ColumnNode) header.R;
+        for(int i = 0; i < go; i++) ret = (ColumnNode) ret.R;
+        return ret;
+    }
+
+    private void printBoard(){ // diagnostics to have a look at the board state
+        System.out.println("Board Config: ");
+        for(ColumnNode tmp = (ColumnNode) header.R; tmp != header; tmp = (ColumnNode) tmp.R){
+
+            for(DancingNode d = tmp.D; d != tmp; d = d.D){
+                String ret = "";
+                ret += d.C.name + " --> ";
+                for(DancingNode i = d.R; i != d; i = i.R){
+                    ret += i.C.name + " --> ";
+                }
+                System.out.println(ret);
+            }
+        }
+    }
+
+    // grid is a grid of 0s and 1s to solve the exact cover for
+    // returns the root column header node
+    private ColumnNode makeDLXBoard(int[][] grid){
+        final int COLS = grid[0].length;
+        final int ROWS = grid.length;
+
+        ColumnNode headerNode = new ColumnNode("header");
+        ArrayList<ColumnNode> columnNodes = new ArrayList<ColumnNode>();
+
+        for(int i = 0; i < COLS; i++){
+            ColumnNode n = new ColumnNode(Integer.toString(i));
+            columnNodes.add(n);
+            headerNode = (ColumnNode) headerNode.hookRight(n);
+        }
+        headerNode = headerNode.R.C;
+
+        for(int i = 0; i < ROWS; i++){
+            DancingNode prev = null;
+            for(int j = 0; j < COLS; j++){
+                if (grid[i][j] == 1){
+                    ColumnNode col = columnNodes.get(j);
+                    DancingNode newNode = new DancingNode(col);
+                    if (prev == null)
+                        prev = newNode;
+                    col.U.hookDown(newNode);
+                    prev = prev.hookRight(newNode);
+                    col.size++;
+                }
             }
         }
 
-        return smallestCO;
+        headerNode.size = COLS;
+
+        return headerNode;
+    }
+
+    private void showInfo(){
+        System.out.println("Number of updates: " + updates);
+    }
+
+    // Grid consists solely of 1s and 0s. Undefined behaviour otherwise
+    public DancingLinks(int[][] grid){
+        this(grid, new DefaultHandler());
+    }
+
+    public DancingLinks(int[][] grid, SolutionHandler h){
+        header = makeDLXBoard(grid);
+        handler = h;
+    }
+
+    public void runSolver(){
+        solutions = 0;
+        updates = 0;
+        answer = new LinkedList<DancingNode>();
+        search(0);
+        if(verbose) showInfo();
     }
 
 }
