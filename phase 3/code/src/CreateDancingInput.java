cac0a7346d7a7027ba0eaceca96938f697ec4523
renesteeman <steeman.rene1@gmail.com>
2020-01-17 20:26:11 +0100

    more progress on the input creator

--- phase 3/code/src/CreateDancingInput.java
+++ phase 3/code/src/CreateDancingInput.java
@@ -24,41 +24,60 @@ public class CreateDancingInput {
                     //For every x
                     for(int x=0; x < width; x++){
                         //Check if it fits
-                        if(fits(x, y, z)){
+                        if(fits(x, y, z, shape)){
+                            int shapeWidth = shape[0][0].length;
+                            int shapeHeight = shape[0].length;
+                            int shapeDepth = shape.length;
+
+                            int shapeX = 0;
+                            int shapeY = 0;
+                            int shapeZ = 0;
+
                             //Get shape inside container
                             Integer[][][] shapeInContainer = new Integer[depth][height][width];
 
                             //Place shape in container
-                            for(int zContainer=0; z<depth; z++){
-                                for(int yContainer=0; y < height; y++) {
-                                    for (int xContainer = 0; x < width; x++) {
-                                        //Check if the shape would be here
-                                        System.out.println(x>= xContainer);
-                                        System.out.println(x<=(xContainer+shape[0][0].length));
-                                        System.out.println(y>= yContainer);
-                                        System.out.println(yContainer+shape[0].length);
-                                        System.out.println(z>= zContainer);
-                                        System.out.println(z<=(zContainer+shape.length));
-                                        if(x>= xContainer && x<=(xContainer+shape[0][0].length) && y>= yContainer && y<=(yContainer+shape[0].length) && z>= zContainer && z<=(zContainer+shape.length)){
-//                                            int xShape = xContainer+shape[0][0].length - x;
-//                                            int yShape = yContainer+shape[0].length - y;
-//                                            int zShape = zContainer+shape.length - z;
-//
-//                                            int xIndex = xShape-1;
-//                                            int yIndex = yShape-1;
-//                                            int zIndex = zShape-1;
+                            for(int zContainer=0; z < depth; z++){
+                                shapeY = 0;
 
-                                            if(shape[z][y][x] == 1){
-                                                shapeInContainer[zContainer][yContainer][xContainer] = 1;
-                                            } else {
-                                                shapeInContainer[zContainer][yContainer][xContainer] = 0;
+                                //If this is the layer that the shape needs to be placed in
+                                if(z>= zContainer && z<=(zContainer+shape.length)){
+                                    shapeX = 0;
+
+                                    for(int yContainer=0; y < height; y++) {
+
+                                        //If this is the height that the shape needs to be placed on
+                                        if(y>= yContainer && y<=(yContainer+shape[0].length)){
+                                            for (int xContainer = 0; x < width; x++) {
+
+//                                        System.out.println(x>= xContainer);
+//                                        System.out.println(x<=(xContainer+shape[0][0].length));
+//                                        System.out.println(y>= yContainer);
+//                                        System.out.println(yContainer+shape[0].length);
+//                                        System.out.println(z>= zContainer);
+//                                        System.out.println(z<=(zContainer+shape.length));
+
+                                                //If this is the x that the shape needs to be placed on
+                                                if(x>= xContainer && x<=(xContainer+shape[0][0].length)){
+                                                    if(shape[shapeZ][shapeY][shapeX] == 1){
+                                                        shapeInContainer[zContainer][yContainer][xContainer] = 1;
+                                                    } else {
+                                                        shapeInContainer[zContainer][yContainer][xContainer] = 0;
+                                                    }
+
+                                                    ++shapeX;
+
+                                                } else {
+                                                    shapeInContainer[zContainer][yContainer][xContainer] = 0;
+                                                }
                                             }
-
-                                        } else {
-                                            shapeInContainer[zContainer][yContainer][xContainer] = 0;
                                         }
                                     }
+
+                                    ++shapeY;
                                 }
+
+                                ++shapeZ;
                             }
 
                             //Save it
@@ -70,19 +89,23 @@ public class CreateDancingInput {
         }
     }
 
-    public boolean fits(int startX, int startY, int startZ){
+    public boolean fits(int startX, int startY, int startZ, int[][][] shape){
+        int shapeWidth = shape[0][0].length;
+        int shapeHeight = shape[0].length;
+        int shapeDepth = shape.length;
+
         //check X
-        if(startX+width > width){
+        if(startX+shapeWidth > width){
             return false;
         }
 
         //check Y
-        if(startY+height > height){
+        if(startY+shapeHeight > height){
             return false;
         }
 
         //check z
-        if(startZ+depth > depth){
+        if(startZ+shapeDepth > depth){
             return false;
         }
 
